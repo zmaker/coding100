33bccbd18900fed1e57707a8ff7352bf62ed613e
zeppelinmaker <paolo.aliverti@gmail.com>
2019-03-10 08:47:39 +0100

    if

---
+++ 064-for-loop/064-for-loop.py
@@ -0,0 +1,19 @@
+print range(0,10)
+
+print range(3,6)
+
+for i in range(0,10):
+    print i
+
+elenco = ['fragola', 'banana', 'mela', 'pera']
+for frutta in elenco:
+    print frutta
+
+
+#sommo elementi in una lista
+numeri = [12, 34, 56, 67, 78]
+somma = 0
+for n in numeri:
+    somma += n
+
+print "somma = ",somma
